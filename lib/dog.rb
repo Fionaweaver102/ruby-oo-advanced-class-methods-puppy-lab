e146c051e63600cecfcfbb3d32987aa3575ad234
Fiona Weaver <fiona.l.weaver@gmail.com>
2020-11-18 12:26:00 -0700

    commit tree 0bd2890cdac8571a674d57d8d47d07784e4a68de

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+class Dog 
+    attr_accessor :name, :save 
+
+    @@all = []
+
+    def initialize(name)
+        @name = name  
+        @save = save
+    end 
+
+    def self.all
+        @@all 
+    end 
+
+    def self.clear_all 
+        @@all.clear 
+    end 
+
+    def self.print_all 
+       @@all.collect do |dog, name|
+       print dog.name + "\n"
+       end 
+    end 
+
+    def save 
+        @@all << self
+    end 
+end 
+    
+
+
+    
